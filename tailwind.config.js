e02f1f911ae6689fc67d572cdae23a2979982b59
Omars.DEV <omarsukarieh99@gmail.com>
2021-11-05 10:58:34 +0200

    configrue color tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 // tailwind.config.js
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        CBackground: "#f8f9fc",
+        CPaper: "#f4f3ee",
+        CDanger: "#b00020",
+        CInfo: "#007bff",
+        CGrey: "#aeaeae",
+        CWhite: "#ffffff",
+        CBlack: "#333333",
+        CSecondary: "#d0dae7",
+        CPrimary: "#f8b916",
+        CPrimaryHover: "#D59B06",
+        CTextSecondary: "#707070",
+        CCard: "#F4F3EE",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
