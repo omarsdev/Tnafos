18d9de17c81f8fd787443042e8167c04268950e2
SARA93-CODER <sara.inbashi@hotmail.com>
2021-11-06 12:21:29 +0200

    save ..

--- src/pages/dashboard/components/Navbar.js
+++ src/pages/dashboard/components/Navbar.js
@@ -1,9 +1,53 @@
 import React from 'react'
+import {Box,
+    IconButton,
+    Image,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuList} from "@chakra-ui/react";
+import { useRouteMatch, Link } from 'react-router-dom';
 
 export const Navbar = () => {
-    return (
-        <div>
-            Based Navbar content here...
-        </div>
+
+    const match = useRouteMatch();
+    return ( 
+              <Box
+                bg="white"
+                className="flex flex-row-reverse h-129.5 opacity-100 shadow-xl w-1920">
+                <div className="flex flex-row items-center">
+                  <div className="font-medium h-25 justify-center my-auto text-gray-700 text-md w-81">
+                    {/* <span className="my-auto mr-7">
+                      {userData.first_name} {userData.last_name}
+                    </span> */}
+                  </div>
+                  <Image
+                    className="object-cover border rounded-full h-14 w-14 border-CPrimary css-0 rounded-ful"
+                    src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZmFjZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
+                    alt="User Profile Photo"
+                    style={{ width: "70px", height: "70px" }}
+                  />
+                  <Menu>
+                    <MenuButton
+                      as={IconButton}
+                    //   rightIcon={<ChevronDownIcon />}
+                      className="w-12 h-6 my-auto mr-5"
+                    />
+                    <MenuList className="h-12 mt-5 bg-white border border-gray-300 rounded opacity-50 w-28">
+                      <Link to={`${match.url}/user/profile`}>
+                        <MenuItem className="hover:bg-gray-200">
+                          My Profile
+                        </MenuItem>
+                      </Link>
+                      <MenuItem
+                        // onClick={handlerLogOut}
+                        className="hover:bg-gray-200"
+                      >
+                        Log out
+                      </MenuItem>
+                    </MenuList>
+                  </Menu>
+                  </div>
+              </Box>
     )
 }
--- src/pages/dashboard/components/Sidebar.js
+++ src/pages/dashboard/components/Sidebar.js
@@ -1,9 +1,153 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  List,
+  ListItem,
+  ListIcon,
+  VStack,
+  StackDivider,
+  HStack,
+  Box,
+  Text,
+  UnorderedList,
+  Button,
+  Divider,
+  Image,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from "@chakra-ui/react";
+import { FaAngleDown } from "react-icons/fa";
+import { Link, useRouteMatch } from "react-router-dom";
+import { SidebarMenu } from "../../../config";
+import { FaHome, FaAngleRight } from "react-icons/fa";
 
-export const Sidebar = () => {
+const Heading = (heading) => {
+  return <span>{SidebarMenu.heading}</span>;
+};
+
+const NavItem = ({ to, icon, title, submenu, heading }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleToggle = () => {
+    setIsActive(!isActive);
+  };
+
+  //*if nav-item is a link, then:
+  if (to) {
     return (
-        <div>
-            sidebar contents here...
-        </div>
-    )
-}
+      <List>
+        <Link to="#" className="">
+          <ListItem>
+            <ListIcon as={FaHome} />
+            {title}
+          </ListItem>
+        </Link>
+      </List>
+    );
+  }
+  return (
+    <List>
+      <Button onClick={handleToggle}>
+        <FaAngleRight />
+        {title}
+      </Button>
+      {isActive && Array.isArray(submenu) && (
+        <UnorderedList>
+          <Heading heading={heading} className="" />
+          {submenu.map((item) => (
+            <NavItem className="" {...item} />
+          ))}
+        </UnorderedList>
+      )}
+    </List>
+  );
+};
+
+export const Sidebar = (id) => {
+  //   const [navIndexClick, setNavIndexClick] = useState(null);
+  let match = useRouteMatch();
+
+  return (
+    <Box>
+      <Box>
+        <HStack>
+          <Image src="assets/icons/TnafosLogo" alt="Tnafos-logo" />
+          <VStack spacing={0} align="stretch">
+            <Text>tnafos</Text>
+            <Text>تنافس</Text>
+          </VStack>
+        </HStack>
+      </Box>
+
+      <Box>
+        <List>
+          {SidebarMenu.map((item, idxxx) => (
+            <Box key={idxxx}>
+              <Divider />
+              <ListItem>
+                <Box>
+                  <Text fontSize="xs">{item.heading}</Text>
+                  {item.id === 0 ? (
+                    <Link to={`${match.url}`} key={idxxx}>
+                      <HStack>
+                        {item.icon && <Text>{item.icon}</Text>}
+                        {item.title && <Text>{item.title}</Text>}
+                      </HStack>
+                    </Link>
+                  ) : (
+                    <Box>
+                      {item.navitem &&
+                        item.navitem.map((ele, idx) => (
+                          <List key={idx}>
+                            <ListItem>
+                              {ele.to && !ele.submenu ? (
+                                <Link
+                                  to={`${match.url}${ele.to}`}
+                                  className="font-medium"
+                                  key={idx}
+                                >
+                                  <Button>
+                                    <HStack>
+                                      <Text>{ele.icon}</Text>
+                                      <Text>{ele.title}</Text>
+                                    </HStack>
+                                  </Button>
+                                </Link>
+                              ) : (
+                                <Menu>
+                                  <>
+                                    <MenuButton
+                                      as={Button}
+                                      rightIcon={<FaAngleDown />}
+                                    >
+                                      {ele.title}
+                                    </MenuButton>
+                                    <MenuList>
+                                      {ele.submenu.map((element, index) => (
+                                        <MenuItem key={index}>
+                                          <Link
+                                            to={`${match.url}${element.to}`}
+                                          >
+                                            {element.title}
+                                          </Link>
+                                        </MenuItem>
+                                      ))}
+                                    </MenuList>
+                                  </>
+                                </Menu>
+                              )}
+                            </ListItem>
+                          </List>
+                        ))}
+                    </Box>
+                  )}
+                </Box>
+              </ListItem>
+            </Box>
+          ))}
+        </List>
+      </Box>
+    </Box>
+  );
+};
