d63ddefc2350c2920ba768083e455fce2159107b
SARA93-CODER <sara.inbashi@hotmail.com>
2021-11-16 13:34:09 +0200

    Invoices page components.

---
+++ src/pages/dashboard/screens/invoices/Incoming.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Incoming = () => {
+  return (
+    <Box>
+      <Heading
+        textColor="gray.600"
+        fontWeight="medium"
+        fontSize="xx-large"
+        fontFamily="inhirit"
+        alignItems="baseline"
+        ml="5"
+      >
+        Invoices - Incoming
+      </Heading>
+    </Box>
+  );
+};
