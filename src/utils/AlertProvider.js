352af6dabdbaf645558cf1a0b623065ac1e731d5
SARA93-CODER <sara.inbashi@hotmail.com>
2021-11-24 14:10:51 +0200

    alerts handling

--- src/utils/AlertProvider.js
+++ src/utils/AlertProvider.js
@@ -1,34 +1,58 @@
-import { Alert } from "components";
-import React from "react";
-import { v4 } from "uuid";
+import { useEffect, useState } from "react";
+import { Box, Text } from "@chakra-ui/react";
+
+export const AlertContext = createContext();
 
 export const AlertProvider = (props) => {
-  const notifications = [
-    {
-      id: v4(),
-      type: "add",
-      message: "Successfully created! ",
-    },
-    {
-      id: v4(),
-      type: "update",
-      message: "Updated successfully!",
-    },
-    {
-      id: v4(),
-      type: "remove",
-      message: "Successfully deleted!",
-    },
-  ];
-  return (
-    <div>
-      <div className="alert-wrapper">
-        {notifications.map((note) => {
-          return <Alert key={note.id} {...note} />;
-        })}
-      </div>
+  const [alert, setAlert] = useState([]);
+  let notifications = [];
 
-      {props.children}
-    </div>
+  const showAlert = () => {
+    const notifications = [
+      (ADD = {
+        id: 1,
+        properties: {
+          type: "add",
+          message: "Successfully created! ",
+          bgColor: "#6EE7B7",
+        },
+      }),
+      (UPDATE = {
+        id: 2,
+        properties: {
+          type: "update",
+          message: "Updated successfully!",
+          bgColor: "#93C5FD",
+        },
+      }),
+      (DELETE = {
+        id: 3,
+        properties: {
+          type: "remove",
+          message: "Successfully deleted!",
+          bgColor: "#F87171",
+        },
+      }),
+    ];
+    setAlert([notifications]);
+  };
+
+  useEffect(() => {
+    showAlert();
+  }, []);
+  return (
+    <AlertContext.Provider value={{}}>{props.childern}</AlertContext.Provider>
   );
 };
+
+{
+  /* <div className="alert-wrapper">
+  {alert.map((note) => {
+    return (
+      <Box key={note.id}>
+        <Text>{note.properties}</Text>
+      </Box>
+    );
+  })}
+</div>; */
+}
