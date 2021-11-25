3e9c652facc94e9e3fb99499a71c4a1662965c22
Omars.DEV <omarsukarieh99@gmail.com>
2021-11-25 21:24:34 +0200

    use Alert COntext

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import { UserDataContextProvider } from "./context";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 import { withFocusVisible } from "@v1v2/chakra";
+import { AlertContextProvider } from "context/AlertContext";
 
 const theme = extendTheme(
   {
@@ -38,16 +39,18 @@ const App = () => {
   return (
     <ChakraProvider theme={theme}>
       <UserDataContextProvider>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Homepage} />
-            <Route path="/register" component={Register} />
-            <Route path="/login" component={Login} />
-            <Route path="/dashboard" component={DashboardLayout} />
-            <Route path="/:search" component={SearchLayout} />
-            <Route path="*" component={E404} />
-          </Switch>
-        </Router>
+        <AlertContextProvider>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Homepage} />
+              <Route path="/register" component={Register} />
+              <Route path="/login" component={Login} />
+              <Route path="/dashboard" component={DashboardLayout} />
+              <Route path="/:search" component={SearchLayout} />
+              <Route path="*" component={E404} />
+            </Switch>
+          </Router>
+        </AlertContextProvider>
       </UserDataContextProvider>
     </ChakraProvider>
   );
---
+++ src/context/AlertContext.js
@@ -0,0 +1,20 @@
+import React, { useState, createContext, useEffect } from "react";
+import { useAlert } from "react-alert";
+
+export const AlertContext = createContext();
+
+export const AlertContextProvider = (props) => {
+  const alertFire = useAlert();
+  const [alert, setAlert] = useState(null);
+
+  useEffect(() => {
+    if (!alert) return;
+    alertFire.show(alert?.message);
+  }, [alert]);
+
+  return (
+    <AlertContext.Provider value={{ alertProviderValue: setAlert }}>
+      {props.children}
+    </AlertContext.Provider>
+  );
+};
