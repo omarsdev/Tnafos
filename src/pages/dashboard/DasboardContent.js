42f84b87a7a93a36e0e1112a2771de31ef24c45a
SARA93-CODER <sara.inbashi@hotmail.com>
2021-12-08 20:36:17 +0200

    build rating card

--- src/pages/dashboard/DasboardContent.js
+++ src/pages/dashboard/DasboardContent.js
@@ -3,28 +3,19 @@ import { Box, Grid } from "@chakra-ui/react";
 
 import { RatingCard } from "./components";
 import { AxiosInstance } from "api/AxiosInstance";
-import { useParams } from "react-router-dom";
+import { useParams, Link, useRouteMatch } from "react-router-dom";
 
 export const DashboardContent = () => {
   const [ratings, setRatings] = useState(null);
 
   const { uuid } = useParams();
+  let match = useRouteMatch();
 
   const getAllRatings = async () => {
     await AxiosInstance.get("/api/dashboard/rating/")
       .then((res) => {
         console.log(res.data.data);
         setRatings(res.data.data);
-      })
-      .catch((err) => {
-        console.log(err.response.data);
-      });
-  };
-
-  const showRating = async () => {
-    await AxiosInstance.get(`/api/dashboard/rating/${uuid}`)
-      .then((res) => {
-        console.log(res.data.data);
       })
       .catch((err) => {
         console.log(err.response.data);
@@ -39,7 +30,9 @@ export const DashboardContent = () => {
     <Box>
       <Grid templateColumns="repeat(3, 1fr)" gap={20} mb="20px">
         {/* {ratings?.map((el, id) => (
-          <RatingCard ratDetails={el} key={id} />
+          <Link to={`${match.url}/rating/${uuid}`} key={id}>
+            <RatingCard ratDetails={el} />
+          </Link>
         ))} */}
         <RatingCard />
       </Grid>
--- src/pages/dashboard/components/index.js
+++ src/pages/dashboard/components/index.js
@@ -1,3 +1,4 @@
 export * from "./Sidebar";
 export * from "./Navbar";
 export * from "./RatingCard";
+export * from "./RatingDetails";
