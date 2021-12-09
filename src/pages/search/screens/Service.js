ec4581c1686dd23d1b85e0a20a155a7e4166abea
Omars.DEV <omarsukarieh99@gmail.com>
2021-12-09 15:33:29 +0200

    unfinte scroll services

--- src/pages/search/screens/Service.js
+++ src/pages/search/screens/Service.js
@@ -6,36 +6,51 @@ import { Box, Grid, Spinner, Text } from "@chakra-ui/react";
 import { AxiosInstance } from "api";
 
 import { CardItem } from "../components";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export const Service = () => {
   const match = useRouteMatch();
-  const [searchResult, setSearchResult] = useState(null);
+  const [searchResult, setSearchResult] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     const handleSearchApi = async () => {
       await AxiosInstance.get(
-        `api/services/search?search=${match.params.search}`
+        `api/services/search?search=${match.params.search}&page=${currentPage}`
       ).then((res) => {
-        setSearchResult(res.data.data);
+        setSearchResult((arr) => arr.concat(res.data.data));
       });
     };
 
     handleSearchApi();
-  }, [match.params.search]);
+  }, [match.params.search, currentPage]);
+
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
 
   return (
     <>
       <Text fontSize="35px">Services</Text>
+      <button onClick={() => console.log(searchResult)}>Click me</button>
       <Box mt="40px">
-        {!searchResult ? (
-          <Spinner size="xl" color="#F8B916" />
-        ) : (
-          <Grid templateColumns="repeat(3, 1fr)" gap={20}>
+        <InfiniteScroll
+          dataLength={searchResult.length}
+          next={handleNextPage}
+          hasMore={true}
+          loader={<Spinner size="xl" color="#F8B916" />}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
+          <Grid templateColumns="repeat(3, 1fr)" gap={10}>
             {searchResult.map((e, i) => (
               <CardItem key={i} data={e} />
             ))}
           </Grid>
-        )}
+        </InfiniteScroll>
       </Box>
     </>
   );
