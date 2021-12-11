981539c74f10c65e19b0fca767af76bd3e311b55
Omars.DEV <omarsukarieh99@gmail.com>
2021-12-11 16:16:16 +0200

    update error local react form in regular control

--- src/components/input/RegularInput.js
+++ src/components/input/RegularInput.js
@@ -76,7 +76,7 @@ export const RegularInputControl = ({
         <RegularInput
           placeholder={placeHolder}
           register={register(`${name}`, { required: true })}
-          error={errors || error}
+          error={error || errors}
           width={width}
           inputType={inputType}
         />
