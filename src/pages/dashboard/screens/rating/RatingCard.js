7d9fe43aa8334b91fa194eb35c9d0eeb1dbda12b
Omars.DEV <omarsukarieh99@gmail.com>
2021-12-13 22:22:56 +0200

    commenct this bug

--- src/pages/dashboard/screens/rating/RatingCard.js
+++ src/pages/dashboard/screens/rating/RatingCard.js
@@ -14,19 +14,19 @@ import {
 import { FiInbox } from "react-icons/fi";
 
 export const RatingCard = () => {
-  const setBorder = () => {
-    let titles = ["1", "2", "3", "4"];
-    switch (titles) {
-      case "1":
-        <Box borderLeftColor="brand.primary" />;
-      case "2":
-        <Box borderLeftColor="brand.info" />;
-      case "3":
-        <Box borderLeftColor="brand.error" />;
-      case "4":
-        <Box borderLeftColor="brand.grey" />;
-    }
-  };
+  // const setBorder = () => {
+  //   let titles = ["1", "2", "3", "4"];
+  //   switch (titles) {
+  //     case "1":
+  //       <Box borderLeftColor="brand.primary" />;
+  //     case "2":
+  //       <Box borderLeftColor="brand.info" />;
+  //     case "3":
+  //       <Box borderLeftColor="brand.error" />;
+  //     case "4":
+  //       <Box borderLeftColor="brand.grey" />;
+  //   }
+  // };
   // const borderColor = [
   //   "brand.primary",
   //   "brand.info",
@@ -43,7 +43,7 @@ export const RatingCard = () => {
       shadow="2xl"
       relative
       bg={"white"}
-      borderLeftColor={setBorder}
+      // borderLeftColor={setBorder}
       borderLeftWidth="4px"
     >
       <HStack w="full">
