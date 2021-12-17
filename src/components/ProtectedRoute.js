f1e1cec3efe0a90e3a96fb2397d0a462fd6aa469
SARA93-CODER <sara.inbashi@hotmail.com>
2021-12-17 20:58:51 +0200

    editing styling stuff

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,16 +1,30 @@
-import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import React, { useState } from "react";
+import { Route, Redirect, useHistory } from "react-router-dom";
+import { AxiosInstance } from "api";
 
-export const ProtectedRoute = ({
-  hasCompany,
-  component: Component,
-  ...rest
-}) => {
+export const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const [companyInfo, setCompanyInfo] = useState(null);
+  let history = useHistory();
+
+  const showCompany = async () => {
+    await AxiosInstance.get("/api/dashboard/company")
+      .then((res) => {
+        setCompanyInfo(res.data.data);
+        let company = res.data.data;
+        console.log(company);
+        delete company.country;
+        delete company.admin;
+        delete company.category;
+      })
+      .catch((err) => {
+        history.push("/dashboard/company");
+      });
+  };
   return (
     <Route
       {...rest}
       render={(props) => {
-        if (hasCompany) {
+        if (companyInfo) {
           return <Component {...props} />;
         } else {
           return (
