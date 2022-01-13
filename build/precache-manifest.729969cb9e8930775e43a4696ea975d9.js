615e1adda4b0d1dbb68bc09195dae7ca1845528d
Omars.DEV <omarsukareih99@gmail.com>
2022-01-13 09:37:06 +0200

    Build react app

---
+++ build/precache-manifest.729969cb9e8930775e43a4696ea975d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54ce92828598671e716da0da209ad466",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98cec663ca14a0931909",
+    "url": "/static/css/main.b6571cff.chunk.css"
+  },
+  {
+    "revision": "607cf3f9861e38d401c8",
+    "url": "/static/js/2.82ab9ad2.chunk.js"
+  },
+  {
+    "revision": "d18dba208164d01c98054a17fb168340",
+    "url": "/static/js/2.82ab9ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98cec663ca14a0931909",
+    "url": "/static/js/main.3268af65.chunk.js"
+  },
+  {
+    "revision": "40f287e22e9f4534dc0c",
+    "url": "/static/js/runtime-main.e328a991.js"
+  },
+  {
+    "revision": "8b23d5b78ff1979d7bb2a27b32a5f70a",
+    "url": "/static/media/login.8b23d5b7.jpg"
+  },
+  {
+    "revision": "93bf23a4a3ccafa40fc33651dfa7a1bf",
+    "url": "/static/media/register.93bf23a4.jpg"
+  }
+]);
