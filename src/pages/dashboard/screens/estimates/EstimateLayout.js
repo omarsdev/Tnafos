ea2cf070a27dedb056634bec5bfcb28d036b3c3a
Omars.DEV <omarsukareih99@gmail.com>
2022-01-16 19:31:05 +0200

    fix EstimateLayout nested route

--- src/pages/dashboard/screens/estimates/EstimateLayout.js
+++ src/pages/dashboard/screens/estimates/EstimateLayout.js
@@ -15,7 +15,6 @@ const EstimateLayout = () => {
     <Switch>
       <Route exact path={`${match.path}`} component={EstimateHome} />
 
-      <Route path={`${match.path}/incoming/:uuid`} component={EstimateCard} />
       <Route
         path={`${match.path}/incoming/:uuid/convert-to-invoice`}
         component={ConvertToInvoice}
@@ -24,18 +23,19 @@ const EstimateLayout = () => {
         path={`${match.path}/incoming/:uuid/update-status`}
         component={UpdateStatus}
       />
+      <Route path={`${match.path}/incoming/:uuid`} component={EstimateCard} />
+      <Route path={`${match.path}/incoming`} component={IncomingEstimates} />
 
-      <Route path={`${match.path}/outgoing/:uuid`} component={EstimateCard} />
-      <Route
-        path={`${match.path}/outgoing/:uuid/convert-to-invoice`}
-        component={ConvertToInvoice}
-      />
       <Route
         path={`${match.path}/outgoing/:uuid/update-status`}
         component={UpdateStatus}
       />
+      <Route
+        path={`${match.path}/outgoing/:uuid/convert-to-invoice`}
+        component={ConvertToInvoice}
+      />
+      <Route path={`${match.path}/outgoing/:uuid`} component={EstimateCard} />
       <Route path={`${match.path}/outgoing`} component={OutgoingEstimates} />
-      <Route path={`${match.path}/incoming`} component={IncomingEstimates} />
     </Switch>
   );
 };
