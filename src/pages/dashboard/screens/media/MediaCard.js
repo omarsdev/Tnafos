58b5ced64554e14e1f6a8660f2e47f2ad63b5399
SARA93-CODER <sara.inbashi@hotmail.com>
2022-01-16 11:50:21 +0200

    creating media page.

---
+++ src/pages/dashboard/screens/media/MediaCard.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Box, Text, VStack, Image, Center } from "@chakra-ui/react";
+import { Link, useRouteMatch, Switch, Route } from "react-router-dom";
+
+import { SecondaryButton } from "../../../../components";
+
+const MediaCard = ({ info }) => {
+  const match = useRouteMatch();
+  return (
+    <Center py="5">
+      <Box
+        className="rounded-3xl shadow-2xl relative bg-white"
+        w="300px"
+        h="250px"
+      >
+        <VStack spacing="20px" mx="5%">
+          <Image
+            src={"https://bit.ly/sage-adebayo"}
+            alt="Segun Adebayo"
+            objectFit="cover"
+            rounded="3xl"
+            w="100%"
+            h="160px"
+            marginTop={"10px"}
+          />
+          <Box className="flex flex-col justify-center">
+            <Text>{info?.file_name}</Text>
+            <Text color="#007BFF">UUID : {info?.uuid} SAR</Text>
+            <Text>{info?.url}</Text>
+          </Box>
+          <Box position="absolute" bottom="5">
+            <Link to={`${match.url}/${info.uuid}`}>
+              <SecondaryButton name="View" />
+            </Link>
+          </Box>
+        </VStack>
+      </Box>
+    </Center>
+  );
+};
+
+export default MediaCard;
