ae2ced2bef8e4b2b95afa4c4e90c678b181f6755
Omars.DEV <omarsukareih99@gmail.com>
2022-01-25 13:17:18 +0200

    Infinte scroll

--- src/pages/search/screens/Service.js
+++ src/pages/search/screens/Service.js
@@ -12,24 +12,32 @@ export const Service = () => {
   const match = useRouteMatch();
   const [searchResult, setSearchResult] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
 
   const handleSearchApi = () => {
     AxiosInstance.get(
       `api/services/search?search=${match.params.search}&page=${currentPage}`
     )
       .then((res) => {
+        if (!res.data.links.next) setHasMore(false);
         setSearchResult((arr) => arr.concat(res.data.data));
+        // console.log(res.data.data);
       })
       .catch((e) => console.log(e));
+    setCurrentPage(currentPage + 1);
   };
 
   useEffect(() => {
     handleSearchApi();
-  }, [match.params.search, currentPage]);
+  }, []);
 
-  const handleNextPage = () => {
-    setCurrentPage(currentPage + 1);
-  };
+  // useEffect(() => {
+  //   // handleSearchApi();
+  // }, [match.params.search, currentPage]);
+
+  // const handleNextPage = () => {
+  //   setCurrentPage(currentPage + 1);
+  // };
 
   return (
     <>
@@ -47,11 +55,23 @@ export const Service = () => {
         }
       >
       </InfiniteScroll> */}
-      <SimpleGrid minChildWidth={"260px"} gap={8}>
-        {searchResult.map((e, i) => (
-          <CardItem key={i} data={e} />
-        ))}
-      </SimpleGrid>
+      <InfiniteScroll
+        dataLength={searchResult.length}
+        next={handleSearchApi}
+        hasMore={hasMore}
+        loader={<Spinner size="xl" color="#F8B916" />}
+        endMessage={
+          <p style={{ textAlign: "center" }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        <SimpleGrid minChildWidth={"260px"} gap={8}>
+          {searchResult.map((e, i) => (
+            <CardItem key={i} data={e} />
+          ))}
+        </SimpleGrid>
+      </InfiniteScroll>
       {/* <Box mt="40px"></Box> */}
       {/* <button onClick={() => console.log(searchResult)}>Click me</button> */}
       {/* <Box mt="40px"> */}
