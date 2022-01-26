c97295ea2e6ee4146166bebe5015423067af4131
SARA93-CODER <sara.inbashi@hotmail.com>
2022-01-26 14:03:54 +0200

    responsive company page.

--- src/pages/dashboard/components/CustomEditForm.js
+++ src/pages/dashboard/components/CustomEditForm.js
@@ -13,19 +13,35 @@ export const CustomEditForm = ({
   return (
     <form pt="5">
       {children}
-      <Flex my="10" w="full" ml="220px">
+      <Flex my="10" w="full" justify="center">
         <PrimaryButton
           name="Update"
           onClick={onUpdate}
           loadingButton={isUpdating}
           buttonType="submit"
           mx="2"
+          width={{ base: 20, sm: 20, md: 32, lg: 36 }}
+          height={{ base: 8, md: 12 }}
+          fontSize={{
+            base: "xx-small",
+            sm: "xx-small",
+            md: "sm",
+            lg: "md",
+          }}
         />
 
         <SecondaryButton
           name="Cancel"
           onClick={onCancelHandler}
           buttonType="button"
+          width={{ base: 20, sm: 20, md: 32, lg: 36 }}
+          height={{ base: 8, md: 12 }}
+          fontSize={{
+            base: "xx-small",
+            sm: "xx-small",
+            md: "sm",
+            lg: "md",
+          }}
         />
         {/* </HStack> */}
       </Flex>
