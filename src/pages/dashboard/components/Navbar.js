84de9f702815d2d94126827ccb8e30ff31d20237
SARA93-CODER <sara.inbashi@hotmail.com>
2022-01-29 14:23:05 +0200

    responsive dashboard components

--- src/pages/dashboard/components/Navbar.js
+++ src/pages/dashboard/components/Navbar.js
@@ -1,14 +1,12 @@
 import React, { useState, useContext, useEffect } from "react";
 import {
   Box,
-  Button,
   Image,
   Menu,
   MenuButton,
   MenuItem,
   MenuList,
   HStack,
-  Text,
 } from "@chakra-ui/react";
 import { useRouteMatch, Link } from "react-router-dom";
 import { FaChevronDown } from "react-icons/fa";
@@ -58,15 +56,33 @@ export const Navbar = () => {
   return (
     <Box
       bg="white"
-      className="flex flex-row-reverse opacity-100 shadow-xl w-full"
-      h="24"
+      flex
+      // flexDirection="row-reverse"
+      shadow="xl"
+      opacity="100"
+      w="full"
+      // className="flex flex-row-reverse opacity-100 shadow-xl w-full"
+      h="20"
     >
       <Box
-        className="css-0 flex flex-row items-center justify-center"
+        // alignItems="center"
+        // placeItems="center"
+        // flex
+        // flexDirection="row"
+        // justify="start"
+        className="css-0 flex flex-row items-center justify-end"
         mr="15px"
       >
-        <HStack className="font-medium h-10 justify-center text-gray-700 text-md w-24">
-          <span className="my-auto ">
+        <HStack
+          // className="font-medium h-10 justify-center text-gray-700 text-md w-24"
+          fontSize={{ base: "xx-small", sm: "xx-small", md: "small", lg: "md" }}
+          textColor="gray.700"
+          justify="center"
+          fontWeight="medium"
+          h={{ base: 2, sm: 4, md: 6, lg: 8 }}
+          w={{ base: 12, sm: 16, md: 20, lg: 24 }}
+        >
+          <span my="auto">
             {userData.first_name} {userData.last_name}
           </span>
         </HStack>
@@ -74,8 +90,8 @@ export const Navbar = () => {
           src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZmFjZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
           alt="User Profile Photo"
           borderRadius="full"
-          boxSize="70px"
-          p="1"
+          boxSize={{ base: 6, sm: 8, md: 16, lg: 20 }}
+          p={{ base: 1, sm: 1, md: 1, lg: 2 }}
           objectFit="cover"
         />
         <Menu isLazy closeOnSelect="true">
@@ -86,16 +102,20 @@ export const Navbar = () => {
               color: "#AEAEAE",
             }}
           >
-            <FaChevronDown />
+            <Box fontSize={{ base: 8, sm: 8, md: 16, lg: 20 }}>
+              <FaChevronDown />
+            </Box>
           </MenuButton>
-          <MenuList w="28" mt="4">
-            <Link to={`${match.url}/user/profile`}>
-              <MenuItem className="hover:bg-gray-200">My Profile</MenuItem>
-            </Link>
-            <MenuItem onClick={handleLogOut} className="hover:bg-gray-200">
-              Log out
-            </MenuItem>
-          </MenuList>
+          <Box fontSize={{ base: 6, sm: 8, md: 16, lg: 20 }}>
+            <MenuList mt="4" w="fit-content">
+              <Link to={`${match.url}/user/profile`}>
+                <MenuItem _hover={{ bg: "gray.200" }}>My Profile</MenuItem>
+              </Link>
+              <MenuItem onClick={handleLogOut} _hover={{ bg: "gray.200" }}>
+                Log out
+              </MenuItem>
+            </MenuList>
+          </Box>
         </Menu>
       </Box>
     </Box>
