c9626bd3f067a7a46cf051de605da5187a217e3f
Omars.DEV <omarsukareih99@gmail.com>
2022-02-04 00:45:19 +0200

    handle sidebar responsive

--- src/pages/dashboard/components/Navbar.js
+++ src/pages/dashboard/components/Navbar.js
@@ -8,15 +8,17 @@ import {
   MenuList,
   HStack,
   Text,
+  Flex,
 } from "@chakra-ui/react";
 import { useRouteMatch, Link } from "react-router-dom";
 import { FaChevronDown } from "react-icons/fa";
+import { AiOutlineMenu } from "react-icons/ai";
 import { removeUserSession } from "../../../utils";
 import { useHistory } from "react-router-dom";
 import { UserDataContext } from "../../../context";
 import { AxiosInstance } from "../../../api";
 
-export const Navbar = () => {
+export const Navbar = ({ handleToggle }) => {
   const { tokenProviderValue, dataProviderValue } = useContext(UserDataContext);
   const { userData, setUserData } = dataProviderValue;
   const { userToken } = tokenProviderValue;
@@ -55,40 +57,48 @@ export const Navbar = () => {
   };
 
   return (
-    <Box
-      bg="white"
-      flex
-      // flexDirection="row-reverse"
+    <Flex
+      bg="brand.white"
       shadow="xl"
       opacity="100"
       w="full"
-      // className="flex flex-row-reverse opacity-100 shadow-xl w-full"
       h="20"
+      flexDir="row"
+      justifyContent="space-between"
+      alignItems="center"
     >
       <Box
+        ml="14px"
+        p="1rem"
+        onClick={handleToggle}
+        cursor="pointer"
+        display={{ base: "block", md: "none" }}
+      >
+        <AiOutlineMenu size="25px" />
+      </Box>
+      <Box
+        h="100%"
         display="flex"
         flexDir="row"
         alignItems="center"
         justifyContent="end"
         mr="15px"
+        w="100%"
       >
-        <HStack
-          fontSize={{ base: "xx-small", sm: "xx-small", md: "small", lg: "md" }}
+        <Text
+          fontSize={{ base: "small", lg: "md" }}
           textColor="gray.700"
-          justify="end"
           fontWeight="medium"
-          h={{ base: 2, sm: 4, md: 6, lg: 8 }}
-          w={{ base: 12, sm: 16, md: 20, lg: 24 }}
+          mr="1rem"
         >
-          <Text my="auto">
-            {userData.first_name} {userData.last_name}
-          </Text>
-        </HStack>
+          {userData.first_name} {userData.last_name}
+        </Text>
+
         <Image
           src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZmFjZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
           alt="User Profile Photo"
           borderRadius="full"
-          boxSize={{ base: 6, sm: 8, md: 16, lg: 20 }}
+          boxSize={{ base: 16, md: 16, lg: 20 }}
           p={{ base: 1, sm: 1, md: 1, lg: 2 }}
           objectFit="cover"
         />
@@ -100,7 +110,7 @@ export const Navbar = () => {
               color: "#AEAEAE",
             }}
           >
-            <Box fontSize={{ base: 8, sm: 8, md: 16, lg: 20 }}>
+            <Box fontSize={{ base: 16, md: 16, lg: 20 }}>
               <FaChevronDown />
             </Box>
           </MenuButton>
@@ -116,6 +126,6 @@ export const Navbar = () => {
           </Box>
         </Menu>
       </Box>
-    </Box>
+    </Flex>
   );
 };
