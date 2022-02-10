6ad9f539cb06d447496a4b20b01f84a7ed26c1be
SARA93-CODER <sara.inbashi@hotmail.com>
2022-02-10 13:40:23 +0200

    add payment form /responsive.

--- src/pages/dashboard/screens/payments/AddPayment.js
+++ src/pages/dashboard/screens/payments/AddPayment.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Box, Heading, HStack } from "@chakra-ui/react";
+import { Box, Heading, HStack, Center } from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
 import { useHistory } from "react-router-dom";
 
@@ -41,11 +41,11 @@ const AddPayment = () => {
     <Box overflowY="scroll" w="full">
       <Box
         px={{ base: 1, md: 2 }}
-        mt={{ base: 1, md: 3 }}
+        mt={{ base: 4, md: 12 }}
         boxShadow="2xl"
         rounded="3xl"
         w={{ base: 230, sm: 340, md: 550, lg: 700 }}
-        ml={{ base: 2, sm: 16, md: 16, lg: 24 }}
+        ml={{ base: 8, sm: 16, md: 16, lg: 32 }}
         bg="white"
       >
         <Heading
@@ -59,79 +59,81 @@ const AddPayment = () => {
           Fill in this form to add new payment
         </Heading>
 
-        <form>
-          <CustomAddForm
-            listForm={[
-              {
-                head: "Enter amount",
-                placeHolder: "Enter amount ",
-                name: "amount",
-                err: err,
-              },
-              {
-                head: "Enter method",
-                placeHolder: "Enter method ",
-                name: "method",
-                err: err,
-              },
-              {
-                head: "Enter Transaction-number ",
-                placeHolder: "Enter Transaction-number",
-                name: "transaction-number",
-                err: err,
-              },
-              {
-                head: "Enter Date  ",
-                placeHolder: "Enter date ",
-                name: "date",
-                err: err,
-              },
-              {
-                head: "Enter invoice-id ",
-                placeHolder: "invoice_id",
-                name: "invoice_id",
-                err: err,
-              },
-              {
-                head: "Enter notes ",
-                placeHolder: "notes",
-                name: "notes",
-                err: err,
-              },
-            ]}
-            control={control}
-            register={register}
-          />
-
-          <HStack spacing="10px" py="10" ml="40">
-            <PrimaryButton
-              name="SAVE"
-              onClick={handleSubmit(createPayment)}
-              loadingButton={isUpdating}
-              width={{ base: 16, sm: 20, md: 32, lg: 36 }}
-              height={{ base: 6, sm: 8, md: 12 }}
-              fontSize={{
-                base: "xx-small",
-                sm: "xx-small",
-                md: "sm",
-                lg: "md",
-              }}
+        <Center mx={{ base: "2%", md: "5%" }} mt="10px">
+          <form>
+            <CustomAddForm
+              listForm={[
+                {
+                  head: "Enter amount",
+                  placeHolder: "Enter amount ",
+                  name: "amount",
+                  err: err,
+                },
+                {
+                  head: "Enter method",
+                  placeHolder: "Enter method ",
+                  name: "method",
+                  err: err,
+                },
+                {
+                  head: "Enter Transaction-number ",
+                  placeHolder: "Enter Transaction-number",
+                  name: "transaction-number",
+                  err: err,
+                },
+                {
+                  head: "Enter Date  ",
+                  placeHolder: "Enter date ",
+                  name: "date",
+                  err: err,
+                },
+                {
+                  head: "Enter invoice-id ",
+                  placeHolder: "invoice_id",
+                  name: "invoice_id",
+                  err: err,
+                },
+                {
+                  head: "Enter notes ",
+                  placeHolder: "notes",
+                  name: "notes",
+                  err: err,
+                },
+              ]}
+              control={control}
+              register={register}
             />
 
-            <SecondaryButton
-              onClick={handleCancel}
-              name="CANCEL"
-              width={{ base: 16, sm: 20, md: 32, lg: 36 }}
-              height={{ base: 6, sm: 8, md: 12 }}
-              fontSize={{
-                base: "xx-small",
-                sm: "xx-small",
-                md: "sm",
-                lg: "md",
-              }}
-            />
-          </HStack>
-        </form>
+            <HStack spacing="5px" py="10" w="full" justify="center">
+              <PrimaryButton
+                name="SAVE"
+                onClick={handleSubmit(createPayment)}
+                loadingButton={isUpdating}
+                width={{ base: 20, sm: 20, md: 32, lg: 36 }}
+                height={{ base: 6, sm: 8, md: 10 }}
+                fontSize={{
+                  base: "xx-small",
+                  sm: "xx-small",
+                  md: "sm",
+                  lg: "md",
+                }}
+              />
+
+              <SecondaryButton
+                onClick={handleCancel}
+                name="CANCEL"
+                width={{ base: 20, sm: 20, md: 32, lg: 36 }}
+                height={{ base: 6, sm: 8, md: 10 }}
+                fontSize={{
+                  base: "xx-small",
+                  sm: "xx-small",
+                  md: "sm",
+                  lg: "md",
+                }}
+              />
+            </HStack>
+          </form>
+        </Center>
       </Box>
     </Box>
   );
