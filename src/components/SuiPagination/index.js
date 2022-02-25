897a9cff021cb99231b6004444f11a54694f8891
SARA93-CODER <sara.inbashi@hotmail.com>
2022-02-25 20:50:39 +0200

    fix bugs

--- src/components/SuiPagination/index.js
+++ src/components/SuiPagination/index.js
@@ -1,4 +1,3 @@
-import React from "react";
 /**
 =========================================================
 * Soft UI Dashboard PRO React - v3.0.0
--- src/pages/dashboard/screens/company/Home.js
+++ src/pages/dashboard/screens/company/Home.js
@@ -1,25 +1,22 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-import { AxiosInstance } from 'api'
+import { AxiosInstance } from "api";
 
 const Home = () => {
-
   const getCompanyInfo = async () => {
-    await AxiosInstance.get("/api/dashboard/company").then((res) => {
-      console.log(res.data.data)
-    }).catch((err) => {
-
-    })
-  }
+    await AxiosInstance.get("/api/dashboard/company")
+      .then((res) => {
+        console.log(res.data.data);
+      })
+      .catch((err) => {});
+  };
 
   useEffect(() => {
     getCompanyInfo();
-  }, [])
+  }, []);
 
-  return (
-    <div>Company Home</div>
-  )
-}
+  return <div>Company Home</div>;
+};
 
 // // Soft UI Dashboard PRO React components
 // import SuiBox from "components/SuiBox";
@@ -186,4 +183,4 @@ const Home = () => {
 //   );
 // };
 
-// export default Home;
+export default Home;
